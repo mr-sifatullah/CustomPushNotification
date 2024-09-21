b99bdcdfada384587236da26734e82403c934600
~sifat~ <sj.sifat99@gmail.com>
2024-09-21 13:03:46 +0600

    Initial commit

---
+++ app/src/main/java/com/sifat/custompushnotification/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.sifat.custompushnotification
+
+import android.app.Application
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
